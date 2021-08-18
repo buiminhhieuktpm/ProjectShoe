2377bdfab62fc015b6ac84553acb21ee7fd8322b
Bui Hieu <gacontapgayno1@gmail.com>
2021-08-18 16:29:45 +0700

    config deploy

--- src/main/java/com/project/domain/GenericPagination.java
+++ src/main/java/com/project/domain/GenericPagination.java
@@ -2,6 +2,5 @@ package com.project.domain;
 
 import org.springframework.data.domain.Page;
 
-public interface GenericPagination<T> {
-    <T> PaginationResult<T> pagination(Page<T> page, Integer pageNo, Integer limit, String sortField, String sortDir);
+public interface GenericPagination<T> {<T> PaginationResult<T> pagination(Page<T> page, Integer pageNo, Integer limit, String sortField, String sortDir);
 }
