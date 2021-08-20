930ba391e2895e40922b483330529279e07afc89
dothetaind00 <70260602+dothetaind00@users.noreply.github.com>
2021-08-21 00:05:49 +0700

    Update ProductRepository.java

--- src/main/java/com/project/repository/ProductRepository.java
+++ src/main/java/com/project/repository/ProductRepository.java
@@ -15,16 +15,16 @@ public interface ProductRepository extends PagingAndSortingRepository<Product, I
 	@Query(value = "Select * from product where name Like %?1% ", nativeQuery = true)
 	Page<Product> findAllByName(String name, Pageable pageable);
 	
-	@Query(value = "select * from shoedatabase.product where datediff(curdate(), oncreate) <= 7 and is_enable = true LIMIT 8", nativeQuery = true)
+	@Query(value = "select * from product where datediff(curdate(), oncreate) <= 7 and is_enable = true LIMIT 8", nativeQuery = true)
 	List<Product> findNewProductByDate();
 	
-	@Query(value = "select * from shoedatabase.product where brand_id = ?1 and is_enable = true  LIMIT ?2", nativeQuery = true)
+	@Query(value = "select * from product where brand_id = ?1 and is_enable = true  LIMIT ?2", nativeQuery = true)
 	List<Product> findByBrand(Integer id, int number);
 	
-	@Query(value = "select * from shoedatabase.product where category_id = ?1 and is_enable = true", nativeQuery = true)
+	@Query(value = "select * from product where category_id = ?1 and is_enable = true", nativeQuery = true)
 	Page<Product> findByCategory(Integer id, Pageable pageable);
 	
-	@Query(value = "select * from shoedatabase.product where category_id Like ?1 and brand_id Like ?2 and name Like ?3 and price between ?4 and ?5 and is_enable = true", nativeQuery = true)
+	@Query(value = "select * from product where category_id Like ?1 and brand_id Like ?2 and name Like ?3 and price between ?4 and ?5 and is_enable = true", nativeQuery = true)
 	Page<Product> filterProduct(String category_id, String brand_id, String keyword, String min, String max, Pageable pageable);
 
 }
